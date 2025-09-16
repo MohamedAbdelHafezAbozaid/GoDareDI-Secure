1ea9ca035e7189caea3250c552f233d67ead172a
MohamedAbdelHafezAbozaid <comm.eng_mohamed@hotmail.com>
2025-09-16 12:50:13 +0300

    Replace old XCFramework with new GoDareDI-XCFramework
    - Remove old GoDareDI.xcframework and build scripts
    - Add new GoDareDI-XCFramework.xcframework with iOS-only support
    - Add build-simple-xcframework.swift for easy rebuilding
    - Support iOS device (arm64) and simulator (arm64, x86_64)
    - Proper swiftinterface files with correct iOS targets
    - Version 1.0.45

--- GoDareDI.xcframework/ios-arm64/GoDareDI.framework/Headers/GoDareDI.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for GoDareDI.
-FOUNDATION_EXPORT double GoDareDIVersionNumber;
-
-//! Project version string for GoDareDI.
-FOUNDATION_EXPORT const unsigned char GoDareDIVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <GoDareDI/PublicHeader.h>
