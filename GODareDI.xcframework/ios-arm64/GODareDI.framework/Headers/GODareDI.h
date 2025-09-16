34a2ebc48c71af6efef9581aa03fe966a8bbf562
MohamedAbdelHafezAbozaid <comm.eng_mohamed@hotmail.com>
2025-09-17 01:47:51 +0300

    Add working GODareDI.xcframework with static libraries

--- GODareDI.xcframework/ios-arm64/GODareDI.framework/Headers/GODareDI.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for GODareDI.
-FOUNDATION_EXPORT double GODareDIVersionNumber;
-
-//! Project version string for GODareDI.
-FOUNDATION_EXPORT const unsigned char GODareDIVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <GODareDI/PublicHeader.h>
