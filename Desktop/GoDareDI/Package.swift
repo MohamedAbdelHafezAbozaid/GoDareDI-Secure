4e52e72912fee475bbe1dd3a7cc3dd5e0ee7fc8b
MohamedAbdelHafezAbozaid <comm.eng_mohamed@hotmail.com>
2025-09-17 14:54:13 +0300

    Release v2.0.11: Updated XCFramework with new checksum

--- Desktop/GoDareDI/Package.swift
+++ Desktop/GoDareDI/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "GoDareDI",
             url: "https://github.com/MohamedAbdelHafezAbozaid/GoDareDI-Secure/releases/download/v2.0.11/GoDareDI-2.0.11.xcframework.zip",
-            checksum: "a9b8b65019240dd112512b7725e6c1e07dcfcc4bdf161636070854f63e88b236"
+            checksum: "63d6b2e5336d116e6de994a6c71293bcbb882ea57890ac3be56ae338a45af501"
         ),
     ]
 )
