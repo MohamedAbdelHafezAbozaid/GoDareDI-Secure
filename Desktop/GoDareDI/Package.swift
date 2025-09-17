3ad6ac93f70cb2f29440d5ca73d12ffde18511c7
MohamedAbdelHafezAbozaid <comm.eng_mohamed@hotmail.com>
2025-09-17 15:44:55 +0300

    Release v2.0.12: Enhanced performance and stability improvements
    - Updated Package.swift with new xcframework URL and checksum
    - Updated release notes with version 2.0.12
    - Built new xcframework with enhanced performance optimizations
    - Updated checksum: aeb9d3723f8edbe706ff230c26dbc11c3641282c97846825c7b9e943bdfcadca

--- Desktop/GoDareDI/Package.swift
+++ Desktop/GoDareDI/Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoDareDI",
-            url: "https://github.com/MohamedAbdelHafezAbozaid/GoDareDI-Secure/releases/download/v2.0.11/GoDareDI-2.0.11.xcframework.zip",
-            checksum: "63d6b2e5336d116e6de994a6c71293bcbb882ea57890ac3be56ae338a45af501"
+            url: "https://github.com/MohamedAbdelHafezAbozaid/GoDareDI-Secure/releases/download/v2.0.12/GoDareDI-2.0.12.xcframework.zip",
+            checksum: "aeb9d3723f8edbe706ff230c26dbc11c3641282c97846825c7b9e943bdfcadca"
         ),
     ]
 )
